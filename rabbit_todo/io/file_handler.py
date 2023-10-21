4f7f412bdf6482007e6511e9f15027affc8586ad
Mayuge <mayuge.studio.public@gmail.com>
2023-10-21 21:11:39 +0900

    [add] add io_utils.py

--- rabbit_todo/io/file_handler.py
+++ rabbit_todo/io/file_handler.py
@@ -4,27 +4,30 @@ FileHandler
 
 # --- Standard Library ---
 import json
-from pathlib import Path
+
+# --- First Party Library ---
+from rabbit_todo.io.io_utils import Directory
+from rabbit_todo.io.io_utils import FileType
 
 
 class FileHandler:
     """Handles file and directory operations for the Rabbit-Todo application."""
 
-    def __init__(self) -> None:
-        self._directory = Path.cwd() / ".rabbit"
-        self._files = ["tasks.json"]
+    def __init__(self, directory: Directory) -> None:
+        self._directory = directory
 
     def initialize(self) -> None:
         """Creates the required directory and files if they don't exist."""
-        self._directory.mkdir(parents=True, exist_ok=True)
+        self._directory.path.mkdir(parents=True, exist_ok=True)
 
-        for file in self._files:
-            file_path = self._directory / file
+        for file in self._directory.children_files:
+            file_path = self._directory.path / file.name
 
             if not file_path.exists():
                 with file_path.open("w", encoding="utf-8") as f:
-                    json.dump({"tasks": []}, f, indent=4, ensure_ascii=False)
+                    if file.type == FileType.JSON:
+                        json.dump(json.loads(file.default_content), f, indent=4, ensure_ascii=False)
 
     def exists(self) -> bool:
         """Returns true if the directory exists, false otherwise."""
-        return self._directory.exists()
+        return self._directory.path.exists()
