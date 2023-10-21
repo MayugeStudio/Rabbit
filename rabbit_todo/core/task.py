02fd01fecfea971d094ffe8643f7c5afdcfdf4ad
Mayuge <mayuge.studio.public@gmail.com>
2023-10-22 02:13:30 +0900

    [feat] Make the from_dict and to_dict methods compatible with changes

--- rabbit_todo/core/task.py
+++ rabbit_todo/core/task.py
@@ -19,6 +19,13 @@ from datetime import datetime
 from typing import Any
 
 
+def _check_str_key(data: dict[str, Any], key: str) -> str:
+    value = data.get(key, None)
+    if not isinstance(value, str):
+        raise ValueError(f"Invalid type for {key}")
+    return value
+
+
 class Task:
     """Represents a single task"""
 
@@ -34,29 +41,36 @@ class Task:
         self._name = name
         self._notes = notes
         self._completed = False
-        self._created_at = created_at if created_at else datetime.now()
-        self._updated_at = updated_at if updated_at else datetime.now()
+        self._created_at = created_at if created_at else datetime.now().replace(microsecond=0)
+        self._updated_at = updated_at if updated_at else datetime.now().replace(microsecond=0)
 
     @classmethod
     def from_dict(cls, data: dict[str, int | str | bool]) -> Task:
         """Instantiates a Task from a dictionary"""
-        id_ = data["id"]
-        name = data["name"]
-        completed = data["completed"]
-        notes = data["notes"]
+        try:
+            id_ = int(data["id"])
+            name = str(data["name"])
+            completed = bool(data["completed"])
+            notes = str(data["notes"])
+            created_at = datetime.strptime(_check_str_key(data, "created_at"), "%Y-%m-%d %H:%M:%S")
+            updated_at = datetime.strptime(_check_str_key(data, "updated_at"), "%Y-%m-%d %H:%M:%S")
+        except (TypeError, KeyError, ValueError) as e:
+            raise ValueError(f"Invalid data for creating a Task: {e}") from e
 
-        assert isinstance(id_, int)
-        assert isinstance(name, str)
-        assert isinstance(completed, bool)
-        assert isinstance(notes, str)
-
-        instance = cls(id_, name, notes=notes)
+        instance = cls(id_, name, notes=notes, created_at=created_at, updated_at=updated_at)
         instance._completed = completed
         return instance
 
-    def to_dict(self) -> dict[str, int | str | bool]:
+    def to_dict(self) -> dict[str, int | str | bool | datetime]:
         """Converts the task to dictionary"""
-        return {"id": self._id, "name": self._name, "completed": self._completed, "notes": self._notes}
+        return {
+            "id": self._id,
+            "name": self._name,
+            "completed": self._completed,
+            "notes": self._notes,
+            "created_at": self._created_at.strftime("%Y-%m-%d %H:%M:%S"),
+            "updated_at": self._updated_at.strftime("%Y-%m-%d %H:%M:%S"),
+        }
 
     @property
     def id(self) -> int:
