642c96fb94cd71af652e52166bb4caf37b190a1e
Mayuge <mayuge.studio.public@gmail.com>
2023-10-21 23:37:11 +0900

    [feat] Implement file_handler.py

--- rabbit_todo/io/file_handler.py
+++ rabbit_todo/io/file_handler.py
@@ -3,31 +3,55 @@ FileHandler
 """
 
 # --- Standard Library ---
-import json
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Generator
+from typing import TextIO
 
 # --- First Party Library ---
-from rabbit_todo.io.io_utils import Directory
-from rabbit_todo.io.io_utils import FileType
+from rabbit_todo.io.io_config.json_config import TASKS_FILE_NAME
+from rabbit_todo.io.io_config.json_config import TASKS_KEY
 
 
 class FileHandler:
     """Handles file and directory operations for the Rabbit-Todo application."""
 
-    def __init__(self, directory: Directory) -> None:
-        self._directory = directory
+    def __init__(self, root: Path) -> None:
+        self._root = root
+        self._tasks_path = root / TASKS_FILE_NAME
 
-    def initialize(self) -> None:
+    def initialize(self, content: str) -> None:
         """Creates the required directory and files if they don't exist."""
-        self._directory.path.mkdir(parents=True, exist_ok=True)
+        self._root.mkdir(parents=True, exist_ok=True)
 
-        for file in self._directory.children_files:
-            file_path = self._directory.path / file.name
+        if not self._tasks_path.exists():
+            with self.open_file_w("tasks") as f:
+                f.write(content)
 
-            if not file_path.exists():
-                with file_path.open("w", encoding="utf-8") as f:
-                    if file.type == FileType.JSON:
-                        json.dump(json.loads(file.default_content), f, indent=4, ensure_ascii=False)
+    @contextmanager
+    def open_file_r(self, target: str) -> Generator[TextIO, None, None]:
+        """Opens the file reading mode."""
+        if target == TASKS_KEY:
+            file = self._tasks_path.open(mode="r", encoding="utf-8")
+            try:
+                yield file
+            finally:
+                file.close()
+        else:
+            raise ValueError
+
+    @contextmanager
+    def open_file_w(self, target: str) -> Generator[TextIO, None, None]:
+        """Opens the file writing mode."""
+        if target == TASKS_KEY:
+            file = self._tasks_path.open(mode="w", encoding="utf-8")
+            try:
+                yield file
+            finally:
+                file.close()
+        else:
+            raise ValueError
 
     def exists(self) -> bool:
         """Returns true if the directory exists, false otherwise."""
-        return self._directory.path.exists()
+        return self._root.exists()
