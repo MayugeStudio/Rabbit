e87bdc32962254d7356ffc51fb81e009f821764f
Mayuge <mayuge.studio.public@gmail.com>
2023-10-31 02:40:15 +0900

    refactor: Refactor json_task_repository.py

--- rabbit_todo/io/json_task_repository.py
+++ rabbit_todo/io/json_task_repository.py
@@ -16,9 +16,9 @@ import json
 from typing import Union
 
 # --- First Party Library ---
-from rabbit_todo.common.messages import ERROR_FILE_CORRUPTED
-from rabbit_todo.common.messages import ERROR_NOT_FOUND
-from rabbit_todo.common.result import Result
+from rabbit_todo.common.error_handler import FILE_CORRUPTED_ERROR_CODE
+from rabbit_todo.common.error_handler import TASK_NOT_FOUND_ERROR_CODE
+from rabbit_todo.common.error_handler import RabbitTodoException
 from rabbit_todo.core.i_task_repository import ITaskRepository
 from rabbit_todo.core.task import Task
 from rabbit_todo.io.file_handler import FileHandler
@@ -33,42 +33,37 @@ class JsonTaskRepository(ITaskRepository):
         self._file_handler = file_handler
         self._file_handler.initialize(initial_content)
 
-    def _load_json(self) -> Result[dict[str, list[dict[str, Union[int, bool, str]]]]]:
+    def _load_json(self) -> dict[str, list[dict[str, Union[int, bool, str]]]]:
         try:
             with self._file_handler.open_file_r(TASKS_KEY) as file:
                 content: dict[str, list[dict[str, Union[int, bool, str]]]] = json.load(file)
-                return Result.ok(content)
+                return content
         except json.decoder.JSONDecodeError:
-            return Result.error(ERROR_FILE_CORRUPTED)
+            raise RabbitTodoException(FILE_CORRUPTED_ERROR_CODE) from None
 
     def _save_tasks(self, tasks: list[Task]) -> None:
         content = {"tasks": [t.to_dict() for t in tasks]}
         with self._file_handler.open_file_w("tasks") as file:
             json.dump(content, file, indent=4, ensure_ascii=False)
 
-    def get_all(self) -> Result[list[Task]]:
-        return self._load_json().map(lambda data: [Task.from_dict(task) for task in data["tasks"]])
+    def get_all(self) -> list[Task]:
+        tasks = self._load_json()
+        return [Task.from_dict(task) for task in tasks["tasks"]]
 
-    def get_by_id(self, task_id: int) -> Result[Task]:
+    def get_by_id(self, task_id: int) -> Task:
         # Get tasks
-        result = self.get_all()
-        if not result.is_success():
-            return Result.error(result)
-        tasks = result.unwrap()
+        tasks = self.get_all()
 
         # Execute
         for task in tasks:
             if task.id == task_id:
-                return Result.ok(task)
+                return task
 
-        return Result.error(ERROR_NOT_FOUND)
+        raise RabbitTodoException(TASK_NOT_FOUND_ERROR_CODE) from None
 
-    def add(self, task: Task) -> Result[bool]:
+    def add(self, task: Task) -> None:
         # Get tasks
-        result = self.get_all()
-        if not result.is_success():
-            return Result.error(result)
-        tasks = result.unwrap()
+        tasks = self.get_all()
 
         # Execute
         tasks.append(task)
@@ -76,30 +71,22 @@ class JsonTaskRepository(ITaskRepository):
         # Save
         self._save_tasks(tasks)
 
-        return Result.ok(True)
-
-    def remove(self, task: Task) -> Result[bool]:
+    def remove(self, task: Task) -> None:
         # Get tasks
-        result = self.get_all()
-        if not result.is_success():
-            return Result.error(result)
-        tasks = result.unwrap()
+        tasks = self.get_all()
 
         # Execute
         for task_ in tasks[:]:
             if task_.id == task.id:
                 tasks.remove(task_)
                 self._save_tasks(tasks)
-                return Result.ok(True)
+                return
 
-        return Result.error(ERROR_NOT_FOUND)
+        raise RabbitTodoException(TASK_NOT_FOUND_ERROR_CODE)
 
-    def update(self, task: Task) -> Result[bool]:
+    def update(self, task: Task) -> None:
         # Get tasks
-        result = self.get_all()
-        if not result.is_success():
-            return Result.error(result)
-        tasks = result.unwrap()
+        tasks = self.get_all()
 
         # Execute
         for task_ in tasks[:]:
@@ -107,6 +94,5 @@ class JsonTaskRepository(ITaskRepository):
                 idx = tasks.index(task_)
                 tasks[idx] = task
                 self._save_tasks(tasks)
-                return Result.ok(True)
 
-        return Result.error(ERROR_NOT_FOUND)
+        raise RabbitTodoException(TASK_NOT_FOUND_ERROR_CODE)
